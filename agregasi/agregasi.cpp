566c92cfd73b65060ae13e36613d8b55fb1a5578
sekar243 <kinasekin6602@gmail.com>
2025-05-26 18:52:53 +0700

    mencetak daftar anak untuk ibu

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 int main()
 {
+    ibu *varIbu = new ibu("dini");
+    ibu *varIbu2 = new ibu("novi");
+    anak *varAnak1 = new anak("tono");
+    anak *varAnak2 = new anak("rini");
+    anak *varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu2->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+    
 
 }
 
