f2509e2cbcf9e03bcd963e7237495abf3f25194d
sekar243 <kinasekin6602@gmail.com>
2025-05-26 18:41:41 +0700

    mencetak daftar anak

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -22,3 +22,13 @@ void ibu::tambahAnak(anak *pAnak)
 {
     daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak()
+{
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    
+    for (auto &a : daftar_anak)
+    {
+        cout << a->nama <<"\n";
+    }
+    cout << endl;
+}
