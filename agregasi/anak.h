4d1cdd2d0d3cf5d1303d33afa6a1632c25721301
sekar243 <kinasekin6602@gmail.com>
2025-05-26 18:21:37 +0700

    memberi akses modifier

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -4,5 +4,14 @@
 
 class anak
 {
-
-}
+public:
+    string nama;
+    anak(string pNama) : nama(pNama)
+    {
+        cout << "Anak \"" << nama << "\"ada\n";
+    }
+    ~anak()
+    {
+        cout << "Anak \"" << nama << "\"tidak ada\n";
+    }
+};
