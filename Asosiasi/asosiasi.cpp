73aba24a3802c203067ea3ac2f44b2ecbc21676d
sekar243 <kinasekin6602@gmail.com>
2025-05-26 21:29:34 +0700

    construktor menampilakn bahwa objek pasien dibuat

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 class dokter;
 class pasien {
-
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    
 };
